30183c0325cecd5f8d56d8d3612a16fffe48373e
Zarathoustra3 <118075069+Zarathoustra3@users.noreply.github.com>
2023-02-04 23:43:42 -0500

    ajout de javascript

---
+++ static/script/set_type_account.js
@@ -0,0 +1,11 @@
+
+let button_1= document.getElementById('button_Prod');
+let type_account = document.getElementById('type_compte');
+let button_Part = document.getElementById('button_Part');
+
+button_1.addEventListener('click', (event) => {
+  type_account.value = 'producteur';
+});
+button_Part.addEventListener('click', (event) => {
+  type_account.value = 'particulier';
+})
