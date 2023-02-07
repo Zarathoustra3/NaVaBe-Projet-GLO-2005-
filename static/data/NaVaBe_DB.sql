3329b87f1e5d44fdf2043396c114ac19b17b3d1f
Zarathoustra3 <118075069+Zarathoustra3@users.noreply.github.com>
2023-02-07 10:59:51 -0500

    Création de la nouvelle base de donnée, et du nouvel utilisateur pour le projet.

--- static/data/NaVaBe_DB.sql
+++ static/data/NaVaBe_DB.sql
@@ -4,7 +4,7 @@ utilisateur se passe convenablement (et l'accord de permissions aussi) :)
 */
 CREATE DATABASE IF NOT EXISTS NAVABE; /*on crée la base de donnée qui sera utilisée pour le projet */
 CREATE USER 'Navabe_Project'@'localhost';                 /*Nouvel utilisateur, cet utilisateur est utilisé dans le code python pour 
-                                                    faire des requetes, donc pas besoin de modifier à chaque mise à jour
+                                                    faire des requêtes, donc pas besoin de modifier à chaque mise à jour
                                                     les identifiants de connection à la BDD ;)*/
 IDENTIFIED WITH caching_sha2_password BY 'GLO-2005';
 GRANT ALL PRIVILEGES ON NAVABE.* TO 'Navabe_Project'@'localhost' WITH GRANT OPTION;
